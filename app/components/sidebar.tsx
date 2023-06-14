8f2baef11b401475597bfb18000f4581c2654c8f
MarktanCCCC <129603094+MarktanCCCC@users.noreply.github.com>
2023-06-14 15:03:04 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>AI-Copy Lab.</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          Make Your Copywriting Batter.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
